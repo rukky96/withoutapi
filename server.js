12b6997cf6fbfb57bf7f01233070634b0b95c76c
Akproko Kelvin <akprokokelvin@gmail.com>
2024-11-21 18:44:17 +0100

    added public folder

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 3000
 
@@ -11,8 +14,13 @@ app.get('/api/users', (req, res) => {
     res.send('These are the users')
 })
 
+app.get("", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", 'home.html'))
+})
+
 app.listen(port, () => {
     console.log("Server is Running")
 })
+
 
 module.exports = app;
